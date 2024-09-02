cf0367725061a0d0b4cea7f542c4aa6ff68802d6
Thefunguy-Github <Dylanadion@outlook.com>
2024-09-01 19:30:14 -0700

    Change one thing

--- Assets/Code/morbuis.gd
+++ Assets/Code/morbuis.gd
@@ -4,5 +4,5 @@ var morb
 
 func bap(sec):
 	await get_tree().create_timer(sec).timeout
-	morb.apply_central_impulse(Vector2(randf_range(-200, 200), randf_range(-250, -400)))
+	morb.apply_central_impulse(Vector2(randf_range(-300, 300), randf_range(-250, -400)))
 	bap(sec)
